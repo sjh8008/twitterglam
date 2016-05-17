e642537cfcb2a9a2c2b0f7d6363d18bf7f39b13b
sarah <sjh741@yahoo.com>
2016-05-16 19:31:14 -0500

    Set up create a new Tweet and show it

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'paperclip'
 
 gem 'puma'
 
+
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!
   def index
   end
 
@@ -9,6 +11,20 @@ class TweetsController < ApplicationController
   end
 
   def new
+    @tweet = Tweet.new
+
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    respond_to do |format|
+          if @tweet.save
+            format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
+          else
+            format.html { render :new }
+          end
+        end
   end
 
   def _forms
